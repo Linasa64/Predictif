5c02ba9fb169b6581668114ffce7031d063b6c26
Lina Borg <lborg@insa-lyon.fr>
2023-05-31 00:03:50 +0200

    CSS Présentation Mediums [Boutons pas alignés]

--- L3421/PREDICTIF_B3421/PROJET PROJECTIF/target/generated-sources/annotations/metier/modele/Client_.java
+++ L3421/PREDICTIF_B3421/PROJET PROJECTIF/target/generated-sources/annotations/metier/modele/Client_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import metier.modele.Consultation;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-05-30T18:35:22")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-05-30T22:35:21")
 @StaticMetamodel(Client.class)
 public class Client_ { 
 
